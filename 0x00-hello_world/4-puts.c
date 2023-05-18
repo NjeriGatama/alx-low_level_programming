04cc752959d5b9f4e311a3dc4397e566937528e6
NjeriGatama <annmacharia905@gmail.com>
2023-05-18 11:31:37 -0700

    task 4 correction two

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - print  a line of code using puts
+ * main - print a line of code using puts
  *
  * Return: always 0
  */
-int main(void)
+int main (void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-
 	return (0);
 }
