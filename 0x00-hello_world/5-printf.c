fbb40905093e1f98790267c8371e2b94c4d0384c
NjeriGatama <annmacharia905@gmail.com>
2023-05-18 11:26:55 -0700

    task 5 correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main = print a string using printf
+ * main - print a string using printf
  *
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
