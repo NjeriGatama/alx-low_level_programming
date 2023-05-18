170769647582b47b27baea41c933dee25c1af9e8
NjeriGatama <annmacharia905@gmail.com>
2023-05-18 11:19:48 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+/**
+ * main = print a string using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome of art,\n");
+
+	return (0);
+}
